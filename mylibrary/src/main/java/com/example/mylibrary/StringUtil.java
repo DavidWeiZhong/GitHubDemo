6416c11137bfa7783b79d0fffc779af3c33ad729
Administrator <1291595004@qq.com>
2017-01-19 17:34:55 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/StringUtil.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+/**
+ * Created by ${邱伟中} on 2017/1/19 0019.下午 5:31
+ */
+public class StringUtil {
+
+    public static String getString(){
+        return "你好";
+    }
+}
